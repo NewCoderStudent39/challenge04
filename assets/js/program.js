ae2673ff9bd72c3aac8ce7bfd6f50aa2e1fa5b25
isaac mcdaniel <ilmcdaniel17@gmail.com>
2023-02-20 18:04:12 -0700

    fix initials not showing

--- assets/js/program.js
+++ assets/js/program.js
@@ -1,14 +1,16 @@
 const countDown = document.querySelector('.quiz-timer');
 const cardQuestion = document.querySelector('.card')
 var seconds = 120;
-var hasTime = true;
-let Score=0;
+var score = 0;
+const maxQuestions = 3;
+var timer;
+var lastTime;
 
 console.log(countDown);
 
 function quizTimer() {
 
-    let timer = setInterval(function() {
+    timer = setInterval(function() {
         seconds--;
         let mins = Math.floor(seconds / 60);
         let sec = seconds % 60;
@@ -17,13 +19,18 @@ function quizTimer() {
 
         countDown.textContent = display;
 
-        if (seconds === 0)
+        if (seconds <= 0)
         {
-            clearInterval(timer);
-            hasTime = false;
-            console.log(hasTime);
+            clearTimer();
+            endGame();
         }
-    } , 1000)    
+    } , 1000)   
+}
+
+function clearTimer() {
+    lastTime = seconds;
+    clearInterval(timer);
+    countDown.textContent = '00:00';
 }
 
 
@@ -36,7 +43,6 @@ const questions = [
             {text: 'Cascading Style Sheet', correct: true},
             {text: 'Container Style Sheet', correct: false},
         ],
-        correct: 'Cascading Style Sheet'
     },
     {
         question: "What Accronym do coders strive to achieve?",
@@ -65,8 +71,14 @@ const questionCard = document.getElementById('question-card');
 const nextButton = document.getElementById('next-button');
 const displayQuestion = document.getElementById('question');
 const possibleAnswers = Array.from(document.getElementsByClassName('answer'));
+const endButton = document.getElementById('end-button');
+const initials = document.querySelector('highscore');
+const initialsInput = document.querySelector('highscore-input');
+const submitHighScore = document.querySelector('submit-button');
 
 startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', setNextQuestion);
+endButton.addEventListener('click', clearBoard)
 
 var questionIndex = 0;
 
@@ -84,27 +96,71 @@ function startGame() {
 
 function setNextQuestion () {
     resetQuiz();
+    addScore();
     showQuestion(); 
-    
+}
+
+function clearBoard() {
+    clearTimer();
+    console.log(lastTime)
+    score = lastTime * 5;
+    console.log(score)
+    resetQuiz();
+    endButton.classList.add('hidden');
+    nextButton.classList.add('hidden');
+    displayQuestion.classList.add('hidden');
+    for (let i = 0; i < possibleAnswers.length; i++) {
+        possibleAnswers[i].classList.add('hidden');
+    }
+    addHighScore();    
+}
+
+function addHighScore() {
+    initials.classList.remove('hidden');
+    initialsInput.classList.remove('hidden');
+    submitHighScore.classList.remove('hidden');
+}
+
+function addScore() { //adding multiple times error
+    score = score + 100;
+    console.log('add score ...');
+    console.log(score);
 }
 
 function showQuestion() {
     displayQuestion.innerText = questions[questionIndex].question;
-    for (let i = 0; i < possibleAnswers.length; i++ ) {
+        for (let i = 0; i < possibleAnswers.length; i++ ) {
         possibleAnswers[i].innerText = questions[questionIndex].answers[i].text;
-        possibleAnswers[i].addEventListener('click', function(event) {
-            if (event.answers[questionIndex].correct) {
-                console.log('success')
-
-                //i want to access questions.answers.correct and check if true 
-                //of false
+        possibleAnswers[i].classList.add(questions[questionIndex].answers[i].correct);
+        possibleAnswers[i].addEventListener('click', function(event){
+            if (event.target.classList.contains('true'))
+            {
+                    event.target.classList.add('correct');
+                    nextButton.classList.remove('hidden');
+                    console.log(questionIndex);
+                    if (questionIndex == maxQuestions) {
+                        endButton.classList.remove('hidden');
+                        nextButton.classList.add('hidden');
+                    }
+            }
+            else {
+                seconds = seconds -10;
+                event.target.classList.add('wrong');
             }
         });
     }
     questionIndex++;
+    console.log(questionIndex);
 }
+
+
 
 function resetQuiz () {
     nextButton.classList.add('hidden');
+    for (let i = 0; i < possibleAnswers.length; i++) {
+        possibleAnswers[i].classList.remove('correct');
+        possibleAnswers[i].classList.remove('wrong');
+        possibleAnswers[i].classList.remove('true');
+        possibleAnswers[i].classList.remove('false');
+    }
 }
-
